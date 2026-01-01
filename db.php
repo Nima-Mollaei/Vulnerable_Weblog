2721de70bccc76aadb58340b1627c4bb1c56124a
UPVEX <101062448+Nima-Mollaei@users.noreply.github.com>
2026-01-02 01:52:24 +0330

    Update db.php

--- db.php
+++ db.php
@@ -12,6 +12,4 @@ $conn = new mysqli($db_servername, $db_username, $db_password, $db_database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// echo "Connected successfully";
 ?>
